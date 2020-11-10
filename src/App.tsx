71e2d0676cc38dd83dc671110cf2a2f270d3beb6
Rogerio Azevedo <rogerio.francis@gmail.com>
2020-11-09 22:32:17 -0400

    add navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,19 @@
+import 'react-native-gesture-handler'
+
 import React from 'react'
 import { Text, View, StatusBar } from 'react-native'
+import { NavigationContainer } from '@react-navigation/native'
+
+import Routes from './routes'
 
 const App: React.FC = () => {
   return (
-    <>
+    <NavigationContainer>
       <StatusBar barStyle="light-content" backgroundColor="#312e38" />
       <View style={{ flex: 1, backgroundColor: '#312e38' }}>
-        <Text>Meu App</Text>
+        <Routes />
       </View>
-    </>
+    </NavigationContainer>
   )
 }
 
--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,14 +1,14 @@
-import React from 'react';
-import {Text} from 'react-native';
+import React from 'react'
+import { Text } from 'react-native'
 
-import {Container} from './styles';
+import { Container } from './styles'
 
 const SignIn: React.FC = () => {
   return (
     <Container>
       <Text>SigIn</Text>
     </Container>
-  );
-};
+  )
+}
 
-export default SignIn;
+export default SignIn
